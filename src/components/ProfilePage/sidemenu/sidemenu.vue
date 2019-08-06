da21fe5a5f6b9b6a91a25cb4d7d490d227339542
CaballeroD <manuelggwp111@gmail.com>
2019-08-06 13:46:32 +0100

    Profile page v.1

---
+++ src/components/ProfilePage/sidemenu/sidemenu.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="containerProfile">
+    <nav class="nav__cont">
+      <ul class="nav">
+        <li class="nav__items homeClass">
+          <i class="fas fa-home"></i>
+          <a href="#">Home</a>
+        </li>
+        <li class="nav__items searchClass">
+          <i class="fas fa-search"></i>
+          <a href="#">Search</a>
+        </li>
+        <li class="nav__items friendsClass">
+          <i class="fas fa-user-friends"></i>
+          <a href="#">Friends</a>
+        </li>
+        <li class="nav__items logoutClass">
+          <i class="fas fa-sign-out-alt"></i>
+          <a href="#">LogOut</a>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <yourBooks></yourBooks>
+      <containerBookFront></containerBookFront>
+      <yourFavouriteBooks></yourFavouriteBooks>
+      <containerBookFrontLiked></containerBookFrontLiked>
+    </main>
+  </div>
+</template>
+
+<script>
+import yourBooks from './yourBooks'
+import yourFavouriteBooks from './yourFavouriteBooks'
+import containerBookFront from './containerBookFront'
+import containerBookFrontLiked from './containerBookFrontLiked'
+export default {
+  name: 'sideMenu',
+  components: {
+    containerBookFront,
+    containerBookFrontLiked,
+    yourFavouriteBooks,
+    yourBooks,
+  },
+}
+</script>
+
+<style scoped>
+.homeClass {
+  grid-area: home;
+}
+
+.searchClass {
+  grid-area: search;
+}
+
+.friendsClass {
+  grid-area: friends;
+}
+
+.logoutClass {
+  grid-area: logout;
+}
+
+.containerProfile {
+  width: 100%;
+  height: 900px;
+}
+
+.nav__cont {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    'home'
+    'search'
+    'friends'
+    'logout';
+  position: fixed;
+  width: 60px;
+  height: calc(100vh);
+  top: 150px;
+  z-index: 100;
+  background-color: #000;
+  overflow: hidden;
+  transition: width 0.3s ease;
+  cursor: pointer;
+  box-shadow: 4px 7px 10px rgba(0, 0, 0, 0.4);
+}
+
+.nav__cont:hover {
+  width: 200px;
+}
+@media screen and (min-width: 600px) {
+  .nav__cont {
+    width: 80px;
+  }
+}
+
+.nav {
+  list-style-type: none;
+  color: white;
+}
+
+.nav:first-child {
+  padding-top: 1.5rem;
+}
+
+.nav__items {
+  padding-bottom: 4rem;
+  font-size: 1rem;
+  font-family: 'Raleway', sans-serif;
+  color: #fb4;
+}
+
+.nav__items a {
+  position: relative;
+  display: block;
+  top: -28px;
+  padding-left: 25px;
+  padding-right: 15px;
+  transition: all 0.3s ease;
+  margin-left: 25px;
+  margin-right: 10px;
+  text-decoration: none;
+  color: #fb1;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 400;
+  font-size: 1.35em;
+}
+
+.nav__items a::after {
+  content: '';
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 2px;
+  background-color: radial-gradient(
+    circle at 94.02% 88.03%,
+    #54a4ff,
+    transparent 100%
+  );
+  transition: all 0.5s ease;
+  z-index: -10;
+}
+
+.nav__items:hover a::after {
+  opacity: 1;
+}
+
+.nav__items svg {
+  width: 26px;
+  height: 26px;
+  position: relative;
+  left: -25px;
+  cursor: pointer;
+}
+
+@media screen and (min-width: 600px) {
+  .nav__items svg {
+    width: 32px;
+    height: 32px;
+    left: -15px;
+  }
+}
+
+h1 {
+  margin-top: 6rem;
+  margin-left: 80px;
+  text-align: center;
+  font-family: 'Roboto';
+  font-weight: 100;
+  font-size: 4rem;
+  text-transform: uppercase;
+  color: white;
+  letter-spacing: 6px;
+  text-shadow: 10px 10px 10px rgba(0, 0, 0, 0.3);
+}
+</style>
--- src/views/book.vue
+++ src/views/book.vue
@@ -1,35 +1,44 @@
 <template>
-  <div class="container">
-    <div class="photoContainer">
-      <img :src="imageUrl" />
-    </div>
+  <div>
+    <cabecera></cabecera>
+    <div class="backgroundContent">
+      <div class="container">
+        <img class="photoContainer" :src="imageUrl" />
+        <div class="contenido">
+          <h1 class="titulo genericStyle">{{title}}</h1>
+          <h3 class="author genericStyle">Author: {{autor}}</h3>
+          <h3 class="rating genericStyle">Rating: {{valoracion}}/10</h3>
+          <h3 class="PublDate genericStyle">Publication date: {{publicationDate}}</h3>
+          <h4 class="description genericStyle">Description: {{descripcion}}</h4>
+          <div class="prices">
+            <h2 class="genericStyle">
+              Price: Now: ${{price}} &nbsp;
+              before: &nbsp;
+              <span
+                class="tachado"
+              >${{price + 5}}</span>
+            </h2>
+          </div>
 
-    <div class="contenido">
-      <h1>{{title}}</h1>
-      <h3>Author: {{autor}}</h3>
-      <h3>Rating: {{valoracion}}/10</h3>
-      <h3>Publication date: {{publicationDate}}</h3>
-      <h4>Description: {{descripcion}}</h4>
-      <div class="prices">
-        <h2>Price: Now: ${{price}} &nbsp;</h2>
-        <h3>before: &nbsp;</h3>
-        <h3 id="tachado">${{price + 5}}</h3>
-      </div>
-
-      <div class="buttons">
-        <button id="like" @click="like">
-          <i class="fa fa-heart"></i>&nbsp;Like
-        </button>
-        <button id="dislike" @click="dislike">
-          <i class="far fa-thumbs-down"></i>&nbsp;Dislike
-        </button>
-        <button @click="buy">Buy</button>
+          <div class="buttons">
+            <button id="like" class="genericStyle" @click="like">
+              <i class="fa fa-heart"></i>&nbsp;Like
+            </button>
+            <button id="dislike" class="genericStyle" @click="dislike">
+              <i class="far fa-thumbs-down"></i>&nbsp;Dislike
+            </button>
+            <button class="genericStyle" @click="buy">Buy</button>
+          </div>
+        </div>
       </div>
     </div>
+    <foot></foot>
   </div>
 </template>
 
 <script>
+import cabecera from '../components/Header/header'
+import foot from '../components/Footer/footer'
 export default {
   data: () => {
     return {
@@ -41,122 +50,144 @@ export default {
         'Lorem Ipsum es simplemente el orem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas',
       valoracion: 10,
       price: 19.9,
-      booksUser:[]
+      booksUser: [],
     }
   },
+  components: { cabecera, foot },
 
   methods: {
     like() {
-      let like = document.querySelector('#like');
-      like.classList.toggle('liked');
-      if(like.classList.contains('liked')){
+      let like = document.querySelector('#like')
+      like.classList.toggle('liked')
+      if (like.classList.contains('liked')) {
         const data = { email: localStorage.getItem('email'), title: this.title }
-        this.$http.post('http://localhost:8081/users/like', data);
+        this.$http.post('http://localhost:8081/users/like', data)
       }
     },
 
-    dislike(){
-      let dislike = document.querySelector('#dislike');
-      let like = document.querySelector('#like');
-      dislike.classList.toggle('liked');
-      like.classList.toggle('liked');
+    dislike() {
+      let dislike = document.querySelector('#dislike')
+      let like = document.querySelector('#like')
+      dislike.classList.toggle('liked')
+      like.classList.toggle('liked')
       const data = { email: localStorage.getItem('email'), title: this.title }
-      this.$http.post('http://localhost:8081/users/dislike', data);
+      this.$http.post('http://localhost:8081/users/dislike', data)
     },
 
     buy() {
-      localStorage.setItem('bookToBuy',this.title);
-      this.$router.push('/buy/');
+      localStorage.setItem('bookToBuy', this.title)
+      this.$router.push('/buy/')
     },
-
   },
-  mounted(){
-    const data = { email: localStorage.getItem('email')};
-    this.$http.post("http://localhost:8081/users/booksUser", data).then(response =>{
-      this.booksUser = response.data.librosFavoritos;
-      let getter = localStorage.getItem('book');
-      
-      for (const book of this.booksUser){
-        if(getter == JSON.stringify(book)){
-          let like = document.querySelector('#like');
-          like.classList.add('liked');
+  mounted() {
+    const data = { email: localStorage.getItem('email') }
+    this.$http
+      .post('http://localhost:8081/users/booksUser', data)
+      .then(response => {
+        this.booksUser = response.data.librosFavoritos
+        let getter = localStorage.getItem('book')
+
+        for (const book of this.booksUser) {
+          if (getter == JSON.stringify(book)) {
+            let like = document.querySelector('#like')
+            like.classList.add('liked')
+          }
         }
-      }
-      
-      if(this.booksUser.includes(book))
-        console.log("le gusta");
-    })
-    let getter = localStorage.getItem('book');
-    const book = JSON.parse(getter);
-    this.imageUrl = book.imageUrl;
-    this.title = book.titulo;
-    this.autor = book.autor;
-    this.publicationDate = book.fechaPublicacion;
-    this.descripcion = book.descripcion;
-  }
+
+        if (this.booksUser.includes(book)) console.log('le gusta')
+      })
+    let getter = localStorage.getItem('book')
+    const book = JSON.parse(getter)
+    this.imageUrl = book.imageUrl
+    this.title = book.titulo
+    this.autor = book.autor
+    this.publicationDate = book.fechaPublicacion
+    this.descripcion = book.descripcion
+  },
 }
 </script>
 
 <style scoped>
-.container {
-  height: 1000px;
+.genericStyle {
+  text-align: center;
+  border: 1px solid #fb1;
+  margin-top: 20px;
+  font-family: 'Raleway', sans-serif;
   width: 100%;
+}
+
+.titulo {
+  grid-area: titulo;
+}
+
+.rating {
+  grid-area: rating;
+}
+
+.PublDate {
+  grid-area: PublDate;
+}
+
+.description {
+  grid-area: description;
+}
+
+.author {
+  grid-area: author;
+}
+
+.buttons {
+  grid-area: buttons;
+  display: flex;
+  flex-direction: column;
+}
+
+.prices {
+  width: 400px;
+  grid-area: price;
+}
+
+.backgroundContent {
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: row;
+}
+
+.container {
+  height: 600px;
+  width: 700px;
+  display: flex;
+  justify-content: center;
 }
 
 .contenido {
   font-family: 'Raleway', sans-serif;
-  background-color: red;
-  height: 70%;
-  width: 30%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
+  height: 700px;
+  width: 350px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    'titulo'
+    'rating'
+    'PublDate'
+    'description'
+    'author'
+    'buttons'
+    'price';
 }
 
 .photoContainer {
-  height: 70%;
-  width: 25%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.7);
+  margin-top: 20px;
+  margin-right: 50px;
+  grid-area: photoContainer;
+  max-height: 559px;
+  width: 100%;
 }
 
-.prices {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-#tachado {
+.tachado {
   text-decoration: line-through;
   text-decoration-color: red;
   color: red;
-}
-
-.buttons {
-  height: 5%;
-  width: 40%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-button {
-  height: 90%;
-  width: 48%;
-  border: 1px solid black;
-  vertical-align: middle;
-  text-align: center;
-  display: inline-block;
-  background-color: green;
-  margin: 2px;
 }
 
 * {
@@ -170,12 +201,5 @@ button {
 button:hover {
   border-color: blue;
   background-color: green;
-}
-
-img {
-  display: inline-block;
-  width: 70%;
-  height: 70%;
-  background-size: cover;
 }
 </style>
